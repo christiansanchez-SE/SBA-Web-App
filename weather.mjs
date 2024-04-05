3f92baefcffdaa0120eb5c3573fa6b220aedab48
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-05 15:30:38 -0400

    Next Commit

--- weather.mjs
+++ weather.mjs
@@ -4,9 +4,18 @@
 export async function getTemp() {
     fetch("https://api.nasa.gov/insight_weather/?api_key=2XYAfxSJ8GTExudstXi0nUVeLhcX8wyIZjPosptI&feedtype=json&ver=1.0")
       .then((response) => response.json())
-      .then((json) => {
+      .then((data) => {
+        const {
+            sol_keys,
+            validity_checks,
+            ...solData
+        } = data
+        const temp = Object.entries(solData).map(([sol, data]) => {
+            return {
+                sol: sol
+            }
+        })
         let container = document.querySelector('.weatherContainer');
-      console.log(json)
-
+        console.log(data)
       });
   }
