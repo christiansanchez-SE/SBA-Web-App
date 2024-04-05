31e93799c367e6bef09c9d326006595891e9a566
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-05 14:09:42 -0400

    Third Commit

--- app.mjs
+++ app.mjs
@@ -1,14 +1,16 @@
 // Main File for Classes and Functions and vars
 // Fetch Request -> JSON placeholder
 
-export async function getPhotos() {
+  export async function getPhotos() {
     fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2XYAfxSJ8GTExudstXi0nUVeLhcX8wyIZjPosptI")
       .then((response) => response.json())
       .then((json) => {
+        for(let i = 0; i < 10; i++){
           let container = document.querySelector('.mainContainer');
-          console.log(json.photos[0].img_src); 
+          console.log(json.photos); 
           let photoAlbums = document.createElement("img");
-          photoAlbums.setAttribute('src', json.photos[0].img_src);
+          photoAlbums.setAttribute('src', json.photos[i].img_src);
           container.appendChild(photoAlbums);
+        }
       });
   }
