fa91ad0ef67265917d464c71b3e45134eab50f0c
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-07 23:03:05 -0400

    finally tocuhes part one

--- data.mjs
+++ data.mjs
@@ -8,10 +8,36 @@ import { gettempTwo } from "./weatherTwo.mjs";
 getTemp();
 gettempTwo();
 
-let searchBtn = document.querySelector(".scrollUp");
+// --------- Night and Day
+
+const body = document.querySelector("body");
+const btn = document.getElementById("btn");
+
+function toggleBtn() {
+    btn.classList.toggle("active");
+    if (btn.classList.contains("active")) {
+        body.style.backgroundImage = 'url("https://windowscustomization.com/wp-content/uploads/2019/05/Firewatch-Cycle.gif?w=300")';
+        body.style.backgroundSize = "cover"
+        body.style.transition = "2s";
+    } else {
+        body.style.background = 'url("https://windowscustomization.com/wp-content/uploads/2018/08/fire-watch.gif")';
+        body.style.backgroundSize = "cover"
+        body.style.backgroundPosition = "center"
+        body.style.transition = "2s";
+    }
+}
+
+btn.addEventListener("click", toggleBtn);
+
+
+
+
+
+// ---------- Scroll up button
+
 
 function toggleButton(){
     window.scrollTo({top: 0, behavior:'smooth'});
 }
-
-searchBtn.addEventListener("click", toggleButton);
+let scrollBtn = document.querySelector(".scrollUp");
+scrollBtn.addEventListener("click", toggleButton);
